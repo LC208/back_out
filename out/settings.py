81bf25509bb968c44085efb0bf25ba53eaeb64df
LC208 <maks.2.03@mail.ru>
2024-10-16 11:52:03 +0800

    change in update.py comment, add django-simplejwt lib

--- out/settings.py
+++ out/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     "rest_framework",
     'base',
     'olddb',
-    'drf_spectacular'
+    'drf_spectacular',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +81,11 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     # YOUR SETTINGS
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+
+    ],
 }
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Your Project API',

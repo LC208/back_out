a4969aa0d577c6757072d22204a80ecc1c51b4e4
LC208 <maks.2.03@mail.ru>
2024-10-17 20:13:18 +0800

    permissions setup

--- olddb/views.py
+++ olddb/views.py
@@ -4,14 +4,19 @@ from rest_framework.generics import (
     CreateAPIView,
     RetrieveUpdateDestroyAPIView,
 )
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny, SAFE_METHODS, BasePermission, IsAuthenticated
 
 from olddb.models import Faculty, Companies
 # from models import Faculty
 from olddb.serializers import CompanySerializer, FacultySerializer
 
 
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in SAFE_METHODS
+
 class FacultyList(ListAPIView):
+    permission_classes = [AllowAny]
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
 
@@ -21,12 +26,14 @@ class FacultyCreateView(CreateAPIView):
     serializer_class = FacultySerializer
 
 
-class FacultySingleView(RetrieveUpdateDestroyAPIView):
+class FacultySingleView(RetrieveUpdateDestroyAPIView): 
+    permission_classes = [IsAdminUser|ReadOnly]
     queryset = Faculty.objects.all()
     serializer_class = FacultySerializer
 
 
 class CompanyListView(ListAPIView):
+    permission_classes = [AllowAny]
     queryset = Companies.objects.all()
     serializer_class = CompanySerializer
 
@@ -38,5 +45,6 @@ class CompanyCreateView(CreateAPIView):
 
 
 class CompanySingleView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAdminUser|ReadOnly]
     queryset = Companies.objects.all()
     serializer_class = CompanySerializer

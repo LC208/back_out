0439a3f750460e577385e7942c2b7ac882fc85d3
LC208 <maks.2.03@mail.ru>
2024-10-19 13:54:12 +0800

    changes in debug settings

--- out/settings.py
+++ out/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY",
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG",
-                       default="False"))
+                       default=False))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 

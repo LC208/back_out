28fcbaa6a2edafc1019f6587c2ccec771b8b5748
LC208 <maks.2.03@mail.ru>
2024-10-30 19:47:54 +0800

    change in models

--- base/models.py
+++ base/models.py
@@ -6,93 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-
-
-class AuthGroup(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(unique=True, max_length=150)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group'
-
-
-class AuthGroupPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_group_permissions'
-        unique_together = (('group', 'permission'),)
-
-
-class AuthPermission(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
-    codename = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_permission'
-        unique_together = (('content_type', 'codename'),)
-
-
-class AuthUser(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    password = models.CharField(max_length=128)
-    last_login = models.DateTimeField(blank=True, null=True)
-    is_superuser = models.BooleanField()
-    username = models.CharField(unique=True, max_length=150)
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    email = models.CharField(max_length=254)
-    is_staff = models.BooleanField()
-    is_active = models.BooleanField()
-    date_joined = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user'
-
-
-class AuthUserGroups(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_groups'
-        unique_together = (('user', 'group'),)
-
-
-class AuthUserUserPermissions(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'auth_user_user_permissions'
-        unique_together = (('user', 'permission'),)
-
-
-class AuthsUserprofile(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    bitrix_user_id = models.IntegerField(blank=True, null=True)
-    mira_id = models.IntegerField(blank=True, null=True)
-    is_student = models.BooleanField()
-    is_teacher = models.BooleanField()
-    user_id = models.IntegerField(unique=True)
-
-    class Meta:
-        managed = False
-        db_table = 'auths_userprofile'
-
-
+from django.contrib.auth.models import User
 
 class Faculty(models.Model):
     name = models.CharField(max_length=255, blank=True, null=True)
@@ -126,7 +40,7 @@ class Companies(models.Model):
     dend = models.CharField(max_length=255, blank=True, null=True)
     agreements = models.CharField(max_length=255, blank=True, null=True)
     image = models.CharField(max_length=1000, blank=True, null=True)
-    user = models.OneToOneField(AuthUser, models.DO_NOTHING, blank=True, null=True)
+    user = models.OneToOneField(User, models.DO_NOTHING, blank=True, null=True)
 
     def __str__(self):
         return self.name 
@@ -187,63 +101,6 @@ class DivisionsInst(models.Model):
         db_table = 'divisions_inst'
 
 
-class DjangoAdminLog(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.IntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
-
-
-
-class GroupScore(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    teacher_score = models.ForeignKey('TeacherScore', models.DO_NOTHING, blank=True, null=True)
-    group = models.ForeignKey('Groups', models.DO_NOTHING, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'group_score'
 
 
 class Groups(models.Model):
@@ -256,14 +113,6 @@ class Groups(models.Model):
         db_table = 'groups'
 
 
-class Migrations(models.Model):
-    id = models.BigAutoField(primary_key=True)
-    migration = models.CharField(max_length=255)
-    batch = models.BigIntegerField()
-
-    class Meta:
-        managed = False
-        db_table = 'migrations'
 
 
 class Profiles(models.Model):

29566f41e9b493805f1bc284063de4b74ab94eb8
OscarISTUedu <f8keakk@gmail.com>
2024-11-04 16:22:28 +0800

    add company_area to CompamySingleViewByToken

--- base/views.py
+++ base/views.py
@@ -213,4 +213,5 @@ class CompamySingleViewByToken(APIView):
                          'last_name':user_selected.last_name,
                          'company_name':compamy_selected.name,
                          'company_image':compamy_selected.image,
+                         'company_area':compamy_selected.area_of_activity,
                          },status=200)
